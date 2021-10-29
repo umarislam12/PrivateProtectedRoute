ad546b625f72f367a27543d3f21e2ebfcb96dcac
Khurram Qureshi <f14b150@ibitpu.edu.pk>
2021-10-29 13:20:27 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AuthExample from './routing';
+
+
+function App() {
+  return (
+    <div className="App">
+     <AuthExample/>
+    </div>
+  );
+}
+
+export default App;
